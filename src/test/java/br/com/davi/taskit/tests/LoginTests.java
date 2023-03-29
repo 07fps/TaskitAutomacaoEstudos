c1df2d1819105cc765285c0bafa28df8a2e3136d
Trico <davir35@gmail.com>
2023-03-28 21:29:57 -0300

    Adicionando o Before e o  After each

--- src/test/java/br/com/davi/taskit/tests/LoginTests.java
+++ src/test/java/br/com/davi/taskit/tests/LoginTests.java
@@ -4,9 +4,7 @@ import br.com.davi.taskit.pages.HomePage;
 import br.com.davi.taskit.pages.RegistroPage;
 import br.com.davi.taskit.pages.SecretaPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,16 +15,20 @@ import java.time.Duration;
 @DisplayName("Testes de novo usuário!")
 public class LoginTests {
 
-    @Test
-    @DisplayName("Registrar um novo usuário")
-    public void testRegistrarUmNovoUsuarioValido() {
+    private WebDriver navegador;
+    @BeforeEach
+    public void setUp(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        WebDriver navegador = new ChromeDriver(options);
-
+        navegador = new ChromeDriver(options);
         navegador.manage().window().maximize();
         navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("Registrar um novo usuário")
+    public void testRegistrarUmNovoUsuarioValido() {
 
         HomePage homePage = new HomePage(navegador);
         homePage.acessarPaginaDoTaskit();
@@ -38,11 +40,10 @@ public class LoginTests {
         SecretaPage secretapage = new SecretaPage(navegador);
         String saudacaoAtual = secretapage.pegarASaudacao();
         Assertions.assertEquals("Hi, Davi Ricardo", saudacaoAtual);
+    }
 
+    @AfterEach
+    public void tearDown(){
         navegador.quit();
-
-
-
-
     }
 }
